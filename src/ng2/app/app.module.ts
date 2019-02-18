ba39b76ef2bf78d6609b585776522255e812515c
tareqkha <tareqkhalid@gmail.com>
2019-02-18 11:14:52 +0100

    un code ajs fonctionne sur une appli a2

--- src/ng2/app/app.module.ts
+++ src/ng2/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent] // No Bootstrap-Component
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) { }
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.body, ['phonecatApp'], { strictDi: true });
+  }
+}
