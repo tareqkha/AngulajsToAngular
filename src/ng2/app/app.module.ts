c7bad090ff0d65b875b2ca7135d61b374709742b
tareqkha <tareqkhalid@gmail.com>
2019-02-18 11:42:54 +0100

    utiliser component A2 dans une template AJS

--- src/ng2/app/app.module.ts
+++ src/ng2/app/app.module.ts
@@ -2,18 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { UpgradeModule, downgradeComponent } from '@angular/upgrade/static';
 import { AppComponent } from './app.component';
+import { Ng2DemoComponent } from "./demo/ng2-demo.component";
 
+declare var angular: any;
+
+angular.module('phonecatApp')
+  .directive(
+    'ng2Demo',
+    downgradeComponent({component: Ng2DemoComponent})
+  );
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ng2DemoComponent
   ],
   imports: [
     BrowserModule,
     UpgradeModule
   ],
+  entryComponents: [
+    Ng2DemoComponent 
+  ],
   providers: [],
-  // bootstrap: [AppComponent] // No Bootstrap-Component
+  // bootstrap: [AppComponent] 
 })
 export class AppModule {
   constructor(private upgrade: UpgradeModule) { }
---
+++ src/ng2/app/demo/ng2-demo.component.ts
@@ -0,0 +1,9 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng2-demo',
+  templateUrl: './ng2-demo.component.html'
+})
+export class Ng2DemoComponent  {
+}
